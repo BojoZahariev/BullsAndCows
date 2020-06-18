2ac207b768aab132015d93030ed09a2d59262d25
Bojo <bozhidar_zahariev@hotmail.com>
2020-06-18 10:41:36 +0100

    first

---
+++ script.js
@@ -0,0 +1,3 @@
+var aiNum = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+var playerNum = [];
+console.log(`ai: ${aiNum}`);
